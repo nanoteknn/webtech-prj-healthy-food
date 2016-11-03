4e1d0193ab7214249879861b3a2b802e4e81af7e
Eduard Budacu <eduard.budacu@gmail.com>
2016-11-03 10:55:10 +0000

    added dashboard and reports modules

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// reference for the static content
 app.use('/admin', express.static('admin'));
 app.use('/pos', express.static('pos'));
+app.use('/reports', express.static('reports'));
+app.use('/dashboard', express.static('dashboard'));
 
+app.get('/', function(req, res) {
+    res.redirect("/dashboard");
+});
+
+// REST methods
 app.get('/products', function(req,res){
     res.status(200).send([{name:"COCA_COLA",price:2.99},{name:"PEPSI",price:2.66}]);
 });
